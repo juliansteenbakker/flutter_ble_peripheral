b4159f5cf1d4f335047852c80f09afdbf0287c69
Julian Steenbakker <juliansteenbakker@outlook.com>
2021-09-23 12:28:24 +0200

    imp: add initial permissions support

--- android/src/main/kotlin/dev/steenbakker/flutter_ble_peripheral/FlutterBlePeripheralPlugin.kt
+++ android/src/main/kotlin/dev/steenbakker/flutter_ble_peripheral/FlutterBlePeripheralPlugin.kt
@@ -6,15 +6,25 @@
 
 package dev.steenbakker.flutter_ble_peripheral
 
+import android.Manifest
+import android.app.Activity
+import android.app.Application
 import android.bluetooth.le.AdvertiseSettings
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
 import androidx.annotation.NonNull
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.*
 
 
-class FlutterBlePeripheralPlugin: FlutterPlugin, MethodChannel.MethodCallHandler, EventChannel.StreamHandler {
+class FlutterBlePeripheralPlugin: FlutterPlugin, MethodChannel.MethodCallHandler,
+  EventChannel.StreamHandler, PluginRegistry.RequestPermissionsResultListener,
+  ActivityAware {
 
   private var methodChannel: MethodChannel? = null
   private var eventChannel: EventChannel? = null
@@ -25,7 +35,6 @@ class FlutterBlePeripheralPlugin: FlutterPlugin, MethodChannel.MethodCallHandler
   private var advertiseCallback: (Boolean) -> Unit = { isAdvertising ->
     eventSink?.success(isAdvertising)
   }
-
 
   /** Plugin registration embedding v2 */
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
@@ -55,8 +64,67 @@ class FlutterBlePeripheralPlugin: FlutterPlugin, MethodChannel.MethodCallHandler
     }
   }
 
+  private val myPermissionCode = 72
+  private var permissionGranted: Boolean = false
+
+  private fun checkPermission() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && currentActivity != null && context != null) {
+      permissionGranted = ContextCompat.checkSelfPermission(context!!,
+        Manifest.permission.BLUETOOTH_ADVERTISE) ==
+              PackageManager.PERMISSION_GRANTED
+      if ( !permissionGranted ) {
+        ActivityCompat.requestPermissions(
+          currentActivity!!,
+          arrayOf(Manifest.permission.BLUETOOTH_ADVERTISE),
+          myPermissionCode )
+      }
+    }
+
+  }
+
+  override fun onRequestPermissionsResult( requestCode: Int,
+                                           permissions: Array<out String>?,
+                                           grantResults: IntArray?): Boolean {
+
+    when (requestCode) {
+      myPermissionCode -> {
+        if ( null != grantResults ) {
+          permissionGranted = grantResults.isNotEmpty() &&
+                  grantResults[0] == PackageManager.PERMISSION_GRANTED
+        }
+        // do something here to handle the permission state
+//        updatePluginState()
+        // only return true if handling the request code
+        return true
+      }
+    }
+    return false
+  }
+
+  private var currentActivity: Activity? = null
+
+  override fun onDetachedFromActivity() {
+    currentActivity = null
+  }
+
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    currentActivity = binding.activity
+    binding.addRequestPermissionsResultListener(this)
+  }
+
+  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    currentActivity = binding.activity
+    binding.addRequestPermissionsResultListener(this)
+  }
+
+  override fun onDetachedFromActivityForConfigChanges() {
+    currentActivity = null
+  }
+
   @Suppress("UNCHECKED_CAST")
   private fun startPeripheral(call: MethodCall, result: MethodChannel.Result) {
+    checkPermission()
+
     if (call.arguments !is Map<*, *>) {
       throw IllegalArgumentException("Arguments are not a map! " + call.arguments)
     }
